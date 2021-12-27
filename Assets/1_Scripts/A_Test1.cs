00b069fe7dfe48ecc7d4793288a5ce774460f0e7
adadff <nogggg2004@naver.com>
2021-12-27 10:28:17 +0900

    2_commit
    test2

---
+++ Assets/1_Scripts/A_Test1.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class A_Test1 : MonoBehaviour
+{
+    public void myTest1(){
+        Debug.Log("test1");
+    }
+    void Start()
+    {
+        
+    }
+
+}
